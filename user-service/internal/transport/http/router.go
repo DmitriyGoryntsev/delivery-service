acc446567a99714acf3a1f0c6691ee2651ce7ac7
Your NameGDA35 <goryntsev510@mail.ru>
2025-08-25 15:46:23 +0300

    feat: router setting

---
+++ user-service/internal/transport/http/router.go
@@ -0,0 +1,50 @@
+package http
+
+import (
+	"context"
+	"fmt"
+	"user-service/internal/config"
+	"user-service/internal/middleware"
+	"user-service/pkg/logger"
+
+	"github.com/labstack/echo/v4"
+)
+
+type RouterConfig struct {
+	Port string
+}
+
+type Router struct {
+	config RouterConfig
+	router *echo.Echo
+}
+
+func NewRouterConfig(cfg config.Config) RouterConfig {
+	return RouterConfig{
+		Port: cfg.HTTPServer.Port,
+	}
+}
+
+func NewRouter(rConfig RouterConfig, logger *logger.Logger) *Router {
+	r := echo.New()
+
+	r.Use(middleware.RequestIDMiddleware())
+	r.Use(middleware.LoggingMiddleware(logger.SugaredLogger))
+
+	return &Router{
+		config: rConfig,
+		router: r,
+	}
+}
+
+func (r *Router) Run() error {
+	return r.router.Start(fmt.Sprintf("%s:%s", r.config.Host, r.config.Port))
+}
+
+func (r *Router) ShuttingDown(ctx context.Context) error {
+	return r.router.Shutdown(ctx)
+}
+
+func (r *Router) Echo() *echo.Echo {
+	return r.router
+}
